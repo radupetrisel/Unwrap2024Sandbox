acd615cbc565da86b9fca55974a5252cd03b0790
Radu Petrisel <radupetrisel@gmail.com>
2024-01-27 16:12:35 +0200

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Radu Petrisel on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
